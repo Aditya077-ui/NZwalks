30c3fe42f6c5e711d6398c56fdcb8fad75cc56bd
Aditya khadka <adityakhadka907@gmail.com>
2024-04-23 16:39:53 +0545

    seeded data for regions and difficulties

--- NZwalks.API/Data/NZwalksDbContext.cs
+++ NZwalks.API/Data/NZwalksDbContext.cs
@@ -17,5 +17,89 @@ namespace NZwalks.API.Data
         public DbSet<Walk> Walks { get; set; }
 
 
-    }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // seed data for difficulties
+            // Easy , Medium , Hard
+            var difficulties = new List<Difficulty>()
+            {
+                new Difficulty()
+                {
+                    Id = Guid.Parse("276c2e0f-24df-45cc-b264-8e7b09c46491"),
+                    Name = "Easy"
+                },
+                new Difficulty()
+                {
+                    Id = Guid.Parse("7b06698e-51a9-4dec-b236-7aeb110edc77"),
+                    Name = "Medium"
+                },
+                new Difficulty()
+                {
+                    Id = Guid.Parse("d4fe12c6-f620-42fb-89ad-991b8d6baa46"),
+                    Name = "Hard"
+                }
+            };
+
+            //seed data to database
+            modelBuilder.Entity<Difficulty>().HasData(difficulties);
+
+
+
+            //seed data for Regions
+
+            var Regions = new List<Region>()
+            {
+               new Region
+                {
+                    Id = Guid.Parse("f7248fc3-2585-4efb-8d1d-1c555f4087f6"),
+                    Name = "Auckland",
+                    Code = "AKL",
+                    RegionImageURl = "https://images.pexels.com/photos/5169056/pexels-photo-5169056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("6884f7d7-ad1f-4101-8df3-7a6fa7387d81"),
+                    Name = "Northland",
+                    Code = "NTL",
+                    RegionImageURl = null
+                },
+                new Region
+                {
+                    Id = Guid.Parse("14ceba71-4b51-4777-9b17-46602cf66153"),
+                    Name = "Bay Of Plenty",
+                    Code = "BOP",
+                    RegionImageURl = null
+                },
+                new Region
+                {
+                    Id = Guid.Parse("cfa06ed2-bf65-4b65-93ed-c9d286ddb0de"),
+                    Name = "Wellington",
+                    Code = "WGN",
+                    RegionImageURl = "https://images.pexels.com/photos/4350631/pexels-photo-4350631.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("906cb139-415a-4bbb-a174-1a1faf9fb1f6"),
+                    Name = "Nelson",
+                    Code = "NSN",
+                    RegionImageURl = "https://images.pexels.com/photos/13918194/pexels-photo-13918194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("f077a22e-4248-4bf6-b564-c7cf4e250263"),
+                    Name = "Southland",
+                    Code = "STL",
+                    RegionImageURl = null
+                },
+            };
+                modelBuilder.Entity<Region>().HasData(Regions);
+
+            }
+        }
+
+
+
+    
 }
